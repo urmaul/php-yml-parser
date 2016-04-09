845efee71049be6f49e5125ecc3460f1d2dcf976
vtlk <vitaliy.mochalov>
2016-04-09 20:43:45 +0600

    book getSetModel

--- src/Sirian/YMLParser/Offer/BookOffer.php
+++ src/Sirian/YMLParser/Offer/BookOffer.php
@@ -4,5 +4,14 @@ namespace Sirian\YMLParser\Offer;
 
 class BookOffer extends Offer
 {
+	public function getModel()
+    {
+        return $this->model;
+    }
 
+    public function setModel($model)
+    {
+        $this->model = $model;
+        return $this;
+    }
 }
